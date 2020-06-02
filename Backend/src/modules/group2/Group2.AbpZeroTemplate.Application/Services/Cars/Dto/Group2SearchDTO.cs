9f67134631ddeb8809a7f9def31fac8aa82b6a89
baotg <16520088@gm.uit.edu.vn>
2020-06-02 21:11:57 +0700

    them 1 cai dto

---
+++ Backend/src/modules/group2/Group2.AbpZeroTemplate.Application/Services/Cars/Dto/Group2SearchDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2.AbpZeroTemplate.Web.Core.Services.Cars.Dto
+{
+    class Group2SearchDTO
+    {
+    }
+}
